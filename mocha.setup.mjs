b34a1099df18bd3e21d2b85d9614860cb5756862
Myrotvorets <support@myrotvorets.center>
2024-01-25 16:01:37 +0200

    chore(dev-deps): remove chai-as-promised

--- mocha.setup.mjs
+++ mocha.setup.mjs
@@ -1,8 +1,6 @@
 import { use } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import chaiSubset from 'chai-subset';
 import chaiThings from 'chai-things';
 
-use(chaiAsPromised);
 use(chaiSubset);
 use(chaiThings);
